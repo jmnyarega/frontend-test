1d1c3be0bb76cfbdbec0f70b72303854b30f4b67
josiahmokob0 <jmnyarega@gmail.com>
2021-12-02 18:46:41 +0300

    move font links to _document.tsx

---
+++ components/Head/index.tsx
@@ -0,0 +1,10 @@
+import Head from "next/head";
+
+const Index = () => (
+  <Head>
+    <title>Store Front</title>
+    <meta name="description" content="Home of the best shoes in town" />
+  </Head>
+);
+
+export default Index;
