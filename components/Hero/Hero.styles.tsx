8f0a1de518fa9e437f5039bc2d6ce9fd444db980
josiahmokob0 <jmnyarega@gmail.com>
2021-12-02 17:24:24 +0300

    style Hero components

--- components/Hero/Hero.styles.tsx
+++ components/Hero/Hero.styles.tsx
@@ -3,9 +3,19 @@
 
 import styled from "styled-components";
 
-export const Hero = styled.div`
+export const HeroStyles = styled.div`
   background-image: url(https://s3.amazonaws.com/nikeinc/assets/65280/FKA_Twigs_Group_Photo_hd_1600.jpg?1484075831);
   background-size: cover;
-  background-position: top left;
+  border-radius: 0.5rem;
   min-height: 15rem;
+
+  @media (min-width: 40rem) {
+    min-height: 20rem;
+  }
+  @media (min-width: 50rem) {
+    min-height: 25rem;
+  }
+  @media (min-width: 75rem) {
+    min-height: 30rem;
+  }
 `;
