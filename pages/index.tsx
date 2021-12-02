40f6586b2995641a4d5d5f5e98f343d0a867e626
josiahmokob0 <jmnyarega@gmail.com>
2021-12-02 17:16:16 +0300

    load fetch data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import type { NextPage } from "next";
+import type { FC } from "react";
 import Head from "next/head";
 // import Image from "next/image";
 import styles from "../styles/Home.module.css";
@@ -7,7 +7,15 @@ import Hero from "../components/Hero";
 import Intro from "../components/Intro";
 import Product from "../components/Products";
 
-const Home: NextPage = () => {
+import { ICategories, IProduct, ISort } from "../types/index.types";
+
+export interface IProps {
+  products: IProduct[];
+  categories: ICategories;
+  sortBy: ISort;
+}
+
+const Home: FC<IProps> = ({ products, categories, sortBy }) => {
   return (
     <div className={styles.container}>
       <Head>
@@ -18,16 +26,39 @@ const Home: NextPage = () => {
           rel="stylesheet"
           href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css"
         />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossorigin={true}
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;500;600&display=swap"
+          rel="stylesheet"
+        />
       </Head>
-      <main className={styles.main}>
-        <Nav />
-        <Hero />
-        <Intro />
-        <Product />
-      </main>
-      <footer className={styles.footer}></footer>
+      <Nav />
+      <Hero />
+      <Intro />
+      <Products products={products} categories={categories} sortBy={sortBy} />
     </div>
   );
 };
+
+export async function getStaticProps() {
+  //runs during the build process
+  const URL = "https://run.mocky.io/v3/5194b007-b9b9-4748-a63f-f5db0ee4cd47";
+  const data = await fetch(URL);
+  const d = await data.json();
+
+  return {
+    props: {
+      products: d.products,
+      categories: d.categories,
+      sortBy: d.sort_by,
+    },
+    revalidate: 1, // updates data every second
+  };
+}
 
 export default Home;
