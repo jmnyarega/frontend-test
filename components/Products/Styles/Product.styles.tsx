92e46a8e10004ac4b3599bd840df51046910ac7c
josiahmokob0 <jmnyarega@gmail.com>
2021-12-02 17:23:49 +0300

    Add styles for product components

--- components/Products/Styles/Product.styles.tsx
+++ components/Products/Styles/Product.styles.tsx
@@ -1,11 +1,47 @@
 import styled from "styled-components";
 
 export const ProductStyle = styled.div`
+  justify-self: center;
   padding: 0.5rem;
-  font-size: 0.75rem;
+  font-size: 0.8575rem;
   max-width: 35ch;
   img {
     background-color: hsl(0, 0%, 80%);
     object-fit: cover;
+  }
+`;
+
+export const ProductInfo = styled.div`
+  min-height: 5rem;
+
+  .price {
+    color: #434344;
+    margin-top: 0.5rem;
+  }
+
+  .name {
+    font-weight: bold;
+  }
+`;
+
+export const ProductButtons = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  gap: 0.5rem;
+
+  button {
+    padding: 0.2em 0.6em;
+    border: 1px solid #dcdcdc;
+    background-color: transparent;
+    border-radius: 0.1em;
+    outline: none;
+    cursor: pointer;
+    font-weight: 500;
+  }
+
+  button:hover,
+  button:focus {
+    opacity: 0.6;
   }
 `;
--- components/Products/Styles/Products.styles.tsx
+++ components/Products/Styles/Products.styles.tsx
@@ -1,12 +1,15 @@
 import styled from "styled-components";
 
 export const ProductsStyle = styled.div`
-  padding: 1rem;
+  padding: 0.5rem;
   display: grid;
-  justify-content: center;
+  justify-content: space-between;
   grid-template-columns: repeat(2, auto);
-
-  @media (min-width: 50em) {
+  @media (min-width: 40rem) {
     grid-template-columns: repeat(3, auto);
+  }
+
+  @media (min-width: 60rem) {
+    grid-template-columns: repeat(4, auto);
   }
 `;
