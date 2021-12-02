7f33a2db794a8da7c0535b00ca72f37256b0272f
josiahmokob0 <jmnyarega@gmail.com>
2021-12-02 13:40:22 +0300

    Add Hero component

---
+++ components/Hero/Hero.styles.tsx
@@ -0,0 +1,11 @@
+/* .hero { */
+/* } */
+
+import styled from "styled-components";
+
+export const Hero = styled.div`
+  background-image: url(https://s3.amazonaws.com/nikeinc/assets/65280/FKA_Twigs_Group_Photo_hd_1600.jpg?1484075831);
+  background-size: cover;
+  background-position: top left;
+  min-height: 15rem;
+`;
---
+++ components/Hero/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Hero } from "./Hero.styles";
+
+function Index() {
+  return <Hero />;
+}
+
+export default Index;
