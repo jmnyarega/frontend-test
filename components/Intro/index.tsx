608f2ad455678d5e254d3ea7c37c754958d538d5
josiahmokob0 <jmnyarega@gmail.com>
2021-12-02 13:41:00 +0300

    Add intro component

---
+++ components/Intro/index.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import {
+  Intro,
+  IntroBadge,
+  IntroTitle,
+  IntroParagraph,
+  IntroSocialLinks,
+} from "./Intro.styles";
+
+function Index() {
+  return (
+    <Intro>
+      <IntroBadge />
+      <IntroTitle>Sneaker DNA</IntroTitle>
+      <IntroParagraph>
+        Home of the best shoes in town. Get free delivery for orders above KES
+        5,000 countrywide.
+      </IntroParagraph>
+      <IntroSocialLinks>
+        <a href="#">Facebook</a>/<a href="#">Instagram</a>/
+        <a href="#">Youtube</a>
+      </IntroSocialLinks>
+    </Intro>
+  );
+}
+
+export default Index;
