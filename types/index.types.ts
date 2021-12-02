a6c0b16b387a017d39b6796418c1c7bafa374743
josiahmokob0 <jmnyarega@gmail.com>
2021-12-02 17:28:46 +0300

    Add types

---
+++ types/index.types.ts
@@ -0,0 +1,14 @@
+export type IProduct = {
+  product_image: string;
+  name: string;
+  price: string;
+};
+
+export type ILoader = {
+  src: string;
+  width: number;
+  quality: number;
+};
+
+export type ICategories = string[];
+export type ISort = string[];
