6c6db56a3d1a8d1b4def00ec5ce30c04b9609cfb
josiahmokob0 <jmnyarega@gmail.com>
2021-12-02 13:43:10 +0300

    Add product components

---
+++ components/Products/Styles/Product.styles.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const ProductStyle = styled.div`
+  padding: 0.5rem;
+  font-size: 0.75rem;
+  max-width: 35ch;
+  img {
+    background-color: hsl(0, 0%, 80%);
+    object-fit: cover;
+  }
+`;
